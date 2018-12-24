5a3b83864272a00fd647939bd271d8f3469582f4
shaggeek <shah.gaurav1710@gmail.com>
2018-12-24 13:46:52 -0500

    Added logger using slf4j and spring boot

--- pmserver/src/main/java/com/cts/pm/main/ProjectManagementMain.java
+++ pmserver/src/main/java/com/cts/pm/main/ProjectManagementMain.java
@@ -1,4 +1,6 @@
 package com.cts.pm.main;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -9,8 +11,11 @@ import org.springframework.context.annotation.ComponentScan;
 @EntityScan( basePackages = {"com.cts.pm"} )
 public class ProjectManagementMain {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(ProjectManagementMain.class);
+	
 	public static void main( String[] args )
     {
+		LOGGER.info("Starting project management app");
     	SpringApplication.run(ProjectManagementMain.class, args);
     }
 }
--- pmserver/src/test/java/pmserver/integration/ParentTaskRepositoryIT.java
+++ pmserver/src/test/java/pmserver/integration/ParentTaskRepositoryIT.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cts.pm.config.ApplicationConfig;
 import com.cts.pm.main.ProjectManagementMain;
@@ -17,6 +18,7 @@ import com.cts.pm.repository.ParentTaskRepositoryDao;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = { ApplicationConfig.class, ProjectManagementMain.class })
+@Transactional
 public class ParentTaskRepositoryIT {
 
 	@Resource(name="parentTaskRepositoryDao")
