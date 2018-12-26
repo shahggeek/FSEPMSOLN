f28b366908bd87d5b1273a762526f36ab8e0bae7
shaggeek <shah.gaurav1710@gmail.com>
2018-12-26 13:49:34 -0500

    Adding components for Header, Task, Project and User

--- pmgui/src/app/app.module.ts
+++ pmgui/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { TaskComponent } from './task/task.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    TaskComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
